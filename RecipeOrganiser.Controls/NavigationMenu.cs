73dfc301087e2e95e5916b7ce9381300f1519160
hsultova <hsultova@users.noreply.github.com>
2021-04-06 16:12:43 +0300

    Add project library for custom controls
    Add NavigationMenu custom control.

---
+++ RecipeOrganiser.Controls/NavigationMenu.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RecipeOrganiser.Controls
+{
+	/// <summary>
+	/// Represents a custom control for the navigation menu. Can be customized with different colors as background, 
+	/// foregrond of the menu items, selection color, selection indicator color, etc...
+	/// </summary>
+	public class NavigationMenu : Control
+	{
+		static NavigationMenu()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(NavigationMenu), new FrameworkPropertyMetadata(typeof(NavigationMenu)));
+		}
+
+		public override void BeginInit()
+		{
+			Items = new List<NavigationMenuItem>();
+			base.BeginInit();
+		}
+
+		/// <summary>
+		/// Navigation menu items.
+		/// </summary>
+		public List<NavigationMenuItem> Items
+		{
+			get { return (List<NavigationMenuItem>)GetValue(ItemsProperty); }
+			set { SetValue(ItemsProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="Items"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ItemsProperty =
+			DependencyProperty.Register("Items", typeof(List<NavigationMenuItem>), typeof(NavigationMenu),
+			new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.AffectsRender | FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+		/// <summary>
+		/// Indicates whether the control is expanded. The default is false.
+		/// </summary>
+		public bool IsExpanded
+		{
+			get { return (bool)GetValue(IsExpandedProperty); }
+			set { SetValue(IsExpandedProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="IsExpanded"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty IsExpandedProperty =
+			DependencyProperty.Register("IsExpanded", typeof(bool), typeof(NavigationMenu), new PropertyMetadata(false));
+
+		/// <summary>
+		/// A brush that describes the foreground of the menu items. The default is White.
+		/// </summary>
+		public Brush MenuItemForeground
+		{
+			get { return (Brush)GetValue(MenuItemForegroundProperty); }
+			set { SetValue(MenuItemForegroundProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="MenuItemForeground"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty MenuItemForegroundProperty =
+			DependencyProperty.Register("MenuItemForeground", typeof(Brush), typeof(NavigationMenu), new PropertyMetadata(Brushes.White));
+
+		/// <summary>
+		/// A brush to color the element on selection (on hover). The default is Gray.
+		/// </summary>
+		public Brush SelectionColor
+		{
+			get { return (Brush)GetValue(SelectionColorProperty); }
+			set { SetValue(SelectionColorProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="SelectionColor"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty SelectionColorProperty =
+			DependencyProperty.Register("SelectionColor", typeof(Brush), typeof(NavigationMenu), new PropertyMetadata(Brushes.Gray));
+
+		/// <summary>
+		/// A brush to color the icon of the button used for collapsing the menu. The default is White.
+		/// </summary>
+		public Brush ButtonIconColor
+		{
+			get { return (Brush)GetValue(ButtonIconColorProperty); }
+			set { SetValue(ButtonIconColorProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="ButtonIconColor"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty ButtonIconColorProperty =
+			DependencyProperty.Register("ButtonIconColor", typeof(Brush), typeof(NavigationMenu), new PropertyMetadata(Brushes.White));
+
+		/// <summary>
+		/// A brush that paint the indicator when an item is selected. The default is White.
+		/// </summary>
+		public Brush SelectionIndicatorColor
+		{
+			get { return (Brush)GetValue(SelectionIndicatorColorProperty); }
+			set { SetValue(SelectionIndicatorColorProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="SelectionIndicatorColor"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty SelectionIndicatorColorProperty =
+			DependencyProperty.Register("SelectionIndicatorColor", typeof(Brush), typeof(NavigationMenuItem), new PropertyMetadata(Brushes.White));
+	}
+}
---
+++ RecipeOrganiser.Controls/NavigationMenuItem.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RecipeOrganiser.Controls
+{
+	/// <summary>
+	/// Represents an item used in the navigation menu custom control.
+	/// </summary>
+	public class NavigationMenuItem : ListBoxItem
+	{
+		static NavigationMenuItem()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(NavigationMenuItem), new FrameworkPropertyMetadata(typeof(NavigationMenuItem)));
+		}
+
+		/// <summary>
+		/// ImageSource (Image path) for the menu item icon. The default is null.
+		/// </summary>
+		public ImageSource Icon
+		{
+			get { return (ImageSource)GetValue(IconProperty); }
+			set { SetValue(IconProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="Icon"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty IconProperty =
+			DependencyProperty.Register("Icon", typeof(ImageSource), typeof(NavigationMenuItem), new PropertyMetadata(null));
+
+		/// <summary>
+		/// Text of the the menu item element.
+		/// </summary>
+		public string Text
+		{
+			get { return (string)GetValue(TextProperty); }
+			set { SetValue(TextProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="Text"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty TextProperty =
+			DependencyProperty.Register("Text", typeof(string), typeof(NavigationMenuItem), new PropertyMetadata(string.Empty));
+
+		/// <summary>
+		/// The command to execute when a menu item is selected.
+		/// </summary>
+		public ICommand SelectionCommand
+		{
+			get { return (ICommand)GetValue(SelectionCommandProperty); }
+			set { SetValue(SelectionCommandProperty, value); }
+		}
+
+		/// <summary>
+		/// Identifies <see cref="SelectionCommand"/> dependency property.
+		/// </summary>
+		public static readonly DependencyProperty SelectionCommandProperty =
+			DependencyProperty.Register("SelectionCommand", typeof(ICommand), typeof(NavigationMenuItem), new PropertyMetadata(null));
+	}
+}
