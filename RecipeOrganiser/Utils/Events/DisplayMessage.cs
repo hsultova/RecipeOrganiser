a4da1650dd685cf65a1e76f5cdea07bb4e3e20e2
hsultova <hsultova@users.noreply.github.com>
2021-05-31 16:56:47 +0300

    Create DisplayMessage.cs

---
+++ RecipeOrganiser/Utils/Events/DisplayMessage.cs
@@ -0,0 +1,21 @@
+﻿namespace RecipeOrganiser.Utils.Events
+{
+	/// <summary>
+	/// Type of the DisplayMessage event.
+	/// </summary>
+	public enum DisplayMessageType
+	{
+		None,
+		Error,
+		Info
+	}
+
+	/// <summary>
+	/// Event arguments for  DisplayMessage event.
+	/// </summary>
+	public class DisplayMessageEventArgs
+	{
+		public string Message { get; set; }
+		public DisplayMessageType Тype { get; set; }
+	}
+}
