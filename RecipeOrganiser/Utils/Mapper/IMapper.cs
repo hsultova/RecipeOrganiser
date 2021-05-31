c0791c7f102340eaab22ff994a59fa06694b768e
hsultova <hsultova@users.noreply.github.com>
2021-05-31 16:44:20 +0300

    Add object to object mapper

---
+++ RecipeOrganiser/Utils/Mapper/IMapper.cs
@@ -0,0 +1,16 @@
+﻿namespace RecipeOrganiser.Utils
+{
+	/// <summary>
+	/// Represents an object to object mapper.
+	/// Can be used when needed object mapping between layers, for example mapping between models and view models.
+	/// </summary>
+	public interface IMapper
+	{
+		/// <summary>
+		/// Executes a mapping from source object to destination object.
+		/// </summary>
+		/// <param name="source">Source object to map from.</param>
+		/// <param name="destination">Destination object to map into.</param>
+		void Map(object source, object destination);
+	}
+}
---
+++ RecipeOrganiser/Utils/Mapper/Mapper.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace RecipeOrganiser.Utils
+{
+	/// <summary>
+	/// Represents a pair of corresponding property source and property destination.
+	/// </summary>
+	public class PropertyPair
+	{
+		public PropertyPair(PropertyInfo sourceProperty, PropertyInfo destinationProperty)
+		{
+			SourceProperty = sourceProperty;
+			DestinationProperty = destinationProperty;
+		}
+
+		public PropertyInfo SourceProperty { get; }
+		public PropertyInfo DestinationProperty { get; }
+	}
+
+	///<inheritdoc/>
+	public sealed class Mapper : IMapper
+	{
+		private readonly Dictionary<string, PropertyPair[]> _mappings;
+
+		public Mapper()
+		{
+			_mappings = new Dictionary<string, PropertyPair[]>();
+		}
+
+		///<inheritdoc/>
+		///Property names of the source and destination should match.
+		public void Map(object source, object destination)
+		{
+			Type sourceType = source.GetType();
+			Type destinationType = destination.GetType();
+
+			string key = GetKey(sourceType, destinationType);
+			if (!_mappings.ContainsKey(key))
+			{
+				MapTypes(sourceType, destinationType);
+			}
+
+			PropertyPair[] propertyPairs = _mappings[key];
+			foreach (PropertyPair pair in propertyPairs)
+			{
+				var sourceValue = pair.SourceProperty.GetValue(source);
+				pair.DestinationProperty.SetValue(destination, sourceValue);
+			}
+		}
+
+		private void MapTypes(Type sourceType, Type destinationType)
+		{
+			PropertyInfo[] sourceProperties = sourceType.GetProperties();
+			PropertyInfo[] destinationProperties = destinationType.GetProperties();
+
+			var propertyPairs = new List<PropertyPair>();
+			foreach (PropertyInfo destinationProperty in destinationProperties)
+			{
+				if (destinationProperty.CanWrite)
+				{
+					PropertyInfo sourceProperty = sourceProperties.FirstOrDefault(p => p.Name == destinationProperty.Name);
+					if (sourceProperty == null)
+					{
+						continue;
+					}
+
+					propertyPairs.Add(new PropertyPair(sourceProperty, destinationProperty));
+				}
+			}
+
+			string key = GetKey(sourceType, destinationType);
+			_mappings.Add(key, propertyPairs.ToArray());
+		}
+
+		private string GetKey(Type sourceType, Type destinationType)
+		{
+			return $"{sourceType}_{destinationType}";
+		}
+	}
+}
