538a7ea5bba35b733a602d93a152ce27045da8e2
hsultova <hsultova@users.noreply.github.com>
2021-08-27 14:15:24 +0300

    [Web] Add preview recipe
    When choosing image file for recipe display it

---
+++ RecipeOrganiser.Web/wwwroot/js/preview-recipe.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+	function previewFile(input) {
+		var file = $("input[type=file]").get(0).files[0];
+
+		if (file) {
+			var reader = new FileReader();
+
+			reader.onload = function () {
+				$("#previewImage").attr("src", reader.result);
+			}
+
+			reader.readAsDataURL(file);
+		}
+	}
+
+	$("#imageFile").change(previewFile);
+});
