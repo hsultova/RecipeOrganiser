4c7622620c2c85663b4382ab43e1ceb45c1f6333
hsultova <hsultova@users.noreply.github.com>
2021-08-27 14:17:32 +0300

    [Web] Add CategoryController
    With index and create actions

---
+++ RecipeOrganiser.Web/Controllers/CategoryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RecipeOrganiser.Domain.Models;
+using RecipeOrganiser.Domain.Services.Abstract;
+using RecipeOrganiser.Web.Models;
+
+namespace RecipeOrganiser.Web.Controllers
+{
+	public class CategoryController : Controller
+	{
+		private readonly ICategoryService _categoryService;
+
+		public CategoryController(ICategoryService categoryService)
+		{
+			_categoryService = categoryService;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Create()
+		{
+			var model = new CategoryViewModel();
+			return View(model);
+		}
+
+		// POST: RecipeController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Create(CategoryViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				//ToDO: Create RecipeIngredients
+				_categoryService.Create(model.Name, model.Description);
+
+				return RedirectToAction(nameof(Index));
+
+			}
+
+			return View(model);
+		}
+	}
+}
